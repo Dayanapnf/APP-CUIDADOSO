50adad4c467088931d677c97cc1df096beac2f35
Felipe Sousa <felipess.dev@gmail.com>
2023-05-06 21:34:51 -0300

    app-cuidadoso

---
+++ CuidadosoApp/app/src/main/java/com/example/cuidadosoapp/Auth/FormCadastro.kt
@@ -0,0 +1,12 @@
+package br.quixada.ufc.cuidadosoapp.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cuidadosoapp.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
---
+++ CuidadosoApp/app/src/main/java/com/example/cuidadosoapp/Auth/FormLogin.kt
@@ -0,0 +1,15 @@
+package br.quixada.ufc.cuidadosoapp.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cuidadosoapp.R
+
+class FormLogin : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
