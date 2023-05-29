0473865e54c27e9db92794bc83d5a294327fb712
Dayana Priscilla <daypnfsi@gmail.com>
2023-05-29 19:31:07 -0300

    upd

---
+++ CuidadosoApp/app/src/main/java/com/example/cuidadosoapp/AddTarefa.java
@@ -0,0 +1,14 @@
+package com.example.cuidadosoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTarefa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tarefa);
+    }
+}
